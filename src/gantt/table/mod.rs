4332e27d257154aacacb5e8f31980d3d9e83c904
a.darafeyeu <a.darafeyeu@gmail.com>
2023-05-05 13:03:56 +0200

    progres draw table

--- src/gantt/table/mod.rs
+++ src/gantt/table/mod.rs
@@ -18,18 +18,120 @@ impl Component for Table {
         }
     }
 
-    fn view(&self, _ctx: &Context<Self>) -> Html {
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let table_content: Html = self.table_props.tasks.clone().unwrap_or_default().iter()
+            .map(|task|  {
+                let expander = if task.hide_children.unwrap_or(true) { "▼" } else { "▶" };   
+                html!{
+                    <div
+                        class="gantt-table-row"
+                        style={format!("height: {}px;", self.table_props.row_height.clone().unwrap() - 2.0)}
+                        key={format!("{}row", task.id.clone().unwrap())}
+                    >
+                        <div
+                        class="gantt-table-cell"
+                        style={format!("min-width: {}; max-width: {}",
+                                self.table_props.row_width.clone().unwrap(),
+                                self.table_props.row_width.clone().unwrap())
+                         }
+                        title={task.name.clone().unwrap()}
+                        >
+                        <div class="gantt-table-name-wrapper">
+                            <div
+                            class={if task.hide_children.unwrap_or(true) { "table-expander" } else { "table-empty-expander" } }
+                            // onClick={() => onExpanderClick(t)}
+                            >
+                                {expander}
+                            </div>
+                            <div>{task.name.clone().unwrap()}</div>
+                        </div>
+                        </div>
+                        <div
+                            class="gantt-table-cell"
+                            style={format!("min-width: {}; max-width: {}",
+                                    self.table_props.row_width.clone().unwrap(),
+                                    self.table_props.row_width.clone().unwrap())
+                            }
+                        >
+                         {task.start.clone().unwrap()}
+                        </div>
+                        <div
+                            class="gantt-table-cell"
+                            style={format!("min-width: {}; max-width: {}",
+                                    self.table_props.row_width.clone().unwrap(),
+                                    self.table_props.row_width.clone().unwrap())
+                            }
+                        >
+                         {task.end.clone().unwrap()}
+                        </div>
+                    </div>
+                }
+            }).collect();
+
+
         html! {
             <div ref={self.task_list_ref.clone()}>
-                // <TaskListHeader {...headerProps} />
+                <div
+                    class="gantt-table"
+                    style={
+                        format!("font-family:{}; font-size:{}", 
+                            self.table_props.font_family.clone().unwrap(),
+                            self.table_props.font_size.clone().unwrap())
+                    }
+                >
+                    <div
+                        class="gantt-table-header"
+                        style={format!("height: {}px;", self.table_props.header_height - 2.0)}
+                    >
+                        <div
+                            class="gantt-table-header-item"       
+                            style={format!("min-width: {};", self.table_props.row_width.clone().unwrap())}
+                        >
+                            {"Name"}
+                        </div>
+                        <div
+                            class="gantt-table-header-separator"
+                            style={format!("height: {}px; margin-top: {}px",
+                                 self.table_props.header_height * 0.5,
+                                 self.table_props.header_height * 0.2)}
+                        />
+                        <div
+                            class="gantt-table-header-item"
+                            style={format!("min-width:{};", self.table_props.row_width.clone().unwrap())}
+                        >
+                            {"From"}
+                        </div>
+                        <div
+                            class="gantt-table-header-separator"
+                            style={format!("height: {}px; margin-top: {}px",
+                                 self.table_props.header_height * 0.5,
+                                 self.table_props.header_height * 0.25)}
+                        />
+                        <div
+                            class="gantt-table-header-item"
+                            style={format!("min-width:{};", self.table_props.row_width.clone().unwrap())}
+                        >
+                            {"To"}
+                        </div>
+                    </div>
+                </div>
                 <div
                     ref={self.horizontal_container_ref.clone()}
                     class="horizontal-container"
-                    // style={ganttHeight ? { height: ganttHeight } : {}}
+                    // style={format!("height:{};", self.table_props.gantt_height)}
                 >
-                // <TaskListTable {...tableProps} />
+                    <div
+                        class="gantt-table-wrapper"
+                        style={
+                            format!("font-family:{}; font-size:{}", 
+                                self.table_props.font_family.clone().unwrap(),
+                                self.table_props.font_size.clone().unwrap())
+                        }
+                    >
+                        {table_content}
+                    </div>
                 </div>
-             </div>
+            </div>
         }
     }
 }
